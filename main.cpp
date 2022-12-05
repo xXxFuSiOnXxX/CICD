ea7a0d35e8cb6fb6348868e30a4d3d9e88862f97
fusion <razgadovdaniil@gmail.com>
2022-12-05 12:38:52 +0300

    what change

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <windows.h>
-#include <cassert>
+
 
 double sum(double a, double b) {
     return a + b;
@@ -15,41 +15,8 @@ double divide(double a, double b) {
     return a / b;
 }
 
-void TestSum() {
-    assert(sum(5, 7) == 12);
-    assert(sum(-5, -7) == -12);
-    assert(sum(-2, 0) == -2);п
-    assert(sum(-5, 5) == 0);
-    std::cout << "TestSum - OK" << '\n';
-}
-void TestDifference() {
-    assert(difference(5, 7) == -2);
-    assert(difference(-5, -7) == 2);
-    assert(difference(-2, 0) == -2);
-    assert(difference(-5, 5) == -10);
-    std::cout << "TestDifference - OK" << '\n';
-}
-void TestMultiplication() {
-    assert(multiplication(5, 7) == 35);
-    assert(multiplication(-5, -7) == 35);
-    assert(multiplication(-2, 0) == 0);
-    assert(multiplication(-5, 5) == -25);
-    std::cout << "TestMultiplication - OK" << '\n';
-}
-void TestDivide() {
-    assert(divide(5, 10) == 0.5);
-    assert(divide(10, 5) == 2);
-    assert(divide(-2, -2) == 1);
-    assert(divide(-5, 5) == -1);
-    std::cout << "TestDivide - OK" << '\n';
-}
-
 int main(){
     SetConsoleOutputCP(CP_UTF8);
-    TestSum();
-    TestDifference();
-    TestMultiplication();
-    TestDivide();
     double x, y, otvet;
     char znak;
     std::cout << "Введите действие(+,-,*,/): ";
